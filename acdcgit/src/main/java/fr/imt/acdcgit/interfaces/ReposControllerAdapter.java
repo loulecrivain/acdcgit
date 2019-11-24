9ad18ff2cf137cfb21e8182bbcd311f0e6dc8918
ugo <43913579+ulecrivain@users.noreply.github.com>
2019-11-24 12:32:30 +0100

    Nouvelle interface et adaptateurs
    Certaines parties n'ont pas été validées
    Des bugs peuvent subsister

---
+++ acdcgit/src/main/java/fr/imt/acdcgit/interfaces/ReposControllerAdapter.java
@@ -0,0 +1,106 @@
+package fr.imt.acdcgit.interfaces;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jgit.transport.CredentialsProvider;
+
+import fr.imt.acdcgit.features.FeatureListFactory;
+import fr.imt.acdcgit.features.RepoFeatures;
+import fr.imt.acdcgit.reposproviders.RepoFileListFromPath;
+import fr.imt.acdcgit.reposproviders.RepoListFromFile;
+import fr.imt.acdcgit.reposproviders.RepoListProvider;
+
+public class ReposControllerAdapter extends RepositoriesAdapter implements ReposControllerInterface {
+	
+	public ReposControllerAdapter(CredentialsProvider cp) {
+		super(cp);
+	}
+
+	/* repos are added all at once */
+	@Override
+	public void addReposFromFile(String filePath) {
+		RepoListProvider<File> fileListProvider = new RepoListFromFile(filePath);
+		FeatureListFactory<RepoFeatures> repoBOFactory = new FeatureListFactory<RepoFeatures>(RepoFeatures.FACTORY,
+				fileListProvider.getRepos(),
+				this.cp);
+		for(RepoFeatures rf : repoBOFactory.get()) {
+			this.path2BO.put(rf.getCanonicalDir(),rf);
+		}
+	}
+
+	@Override
+	public void addReposFromDirectory(String directoryPath) {
+		RepoListProvider<File> fileListProvider = new RepoFileListFromPath(directoryPath);
+		FeatureListFactory<RepoFeatures> repoBOFactory = new FeatureListFactory<RepoFeatures>(RepoFeatures.FACTORY,
+				fileListProvider.getRepos(),
+				this.cp);
+		for(RepoFeatures rf : repoBOFactory.get()) {
+			this.path2BO.put(rf.getCanonicalDir(),rf);
+		}
+	}
+	
+	/* then the internal method for getting BOs is
+	 * overrided
+	 */
+	@Override
+	protected RepoFeatures getBOFromPath(String path, CredentialsProvider credsProvider) {
+		// changing "cache" operation
+		return this.path2BO.get(path); // may be null, ok as it's handled by other methods
+		// I don't change the credsProvider here but this has little impact	}
+		// yes this is hacky
+	}	
+	/* other interface methods are implemented by
+	 * heritage, only the "cache" behaviour differs
+	 * here.
+	 */
+
+
+	@Override
+	public boolean saveReposToFile(String targetedPath) {
+		boolean ok = false;
+		// don't throw any exception, catch them and return false
+		File outfile = new File(targetedPath);
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outfile));
+			for (RepoFeatures rf: this.path2BO.values()) {
+				writer.write(rf.getCanonicalDir());
+				writer.newLine();
+			}
+			writer.close();
+			ok = true;
+		} catch (IOException e) {
+			ok = false;
+		}
+		return ok;
+	}
+
+	@Override
+	public List<String> getRepos() {
+		List<String> l = new ArrayList<String>();
+		for(String s: this.path2BO.keySet()) {
+			l.add(s);
+		}
+		return l;
+	}
+
+	@Override
+	public Map<String, Boolean> pushAll() throws Exception {
+		return this.pushOrPullBatch(true, this.path2BO.keySet().toArray(new String[0]));
+	}
+
+	@Override
+	public Map<String, Boolean> pullAll() throws Exception {
+		return this.pushOrPullBatch(false, this.path2BO.keySet().toArray(new String[0]));
+	}
+
+	@Override
+	public Map<String, String> getAllStates() throws Exception {
+		return this.getState(this.path2BO.keySet().toArray(new String[0]));
+	}
+}
